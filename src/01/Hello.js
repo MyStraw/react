144c26c482c530d6ba831595e1706757c431e3fc
MyStraw <kikongdosa@gmail.com>
2023-04-21 10:20:03 +0900

    주석추가

--- src/01/Hello.js
+++ src/01/Hello.js
@@ -7,7 +7,7 @@ import logo from '../logo.svg';
 const Hello = () => {// 사용자 정의태그엔 반드시 리턴이 있어야. 컴포넌트는 대문자로 시작. Hello. 리턴안에 무조건 태그 들어가야해. 빈 태그 만들자
     return ( // 형제 div 못하고 1개만 만들수 있다 했잖아? 근데 프래그먼트 태그<> 빈태그로 감싸면 가능하다.
         <main className='container'>  
-            <article data-theme="dark">
+            <article data-theme="dark"> 
                 <img src={logo} alt='logo' />
                 < footer >
                     <h1>Hello React!!</h1 >
@@ -24,3 +24,5 @@ export default Hello; // 여기까지가 리액트 컴포넌트(사용자 정의
 //pico 넣으려면 index.html 가서 피코cdn 넣고
 //피코처럼 태그 만들면 된다~
 // 리턴 안에(root) div 형제 2개 만들라면 <></> 빈태그(프래그먼트)로 감싸면 된다.
+
+//img 에는 꼭 alt 달아줘야한다. 이게 표준이래. 이미지가 늦게 생성되거나 안뜰때 보여줄 글자를 넣어야 한다.
