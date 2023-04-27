dc2f728cc26fff488d51a83b7ed6424f75654129
MyStraw <kikongdosa@gmail.com>
2023-04-27 10:12:01 +0900

    4월27일 1교시끝

--- src/02/MyClock.js
+++ src/02/MyClock.js
@@ -1,6 +1,7 @@
 import MyClockImage from "./MyClockImage"; //컴포넌트를 쪼개서 합쳐보자. 이럼 다음에 또 따로 쓸수있다.
 import MyClockTime from "./MyClockTime";
 import '../01/Hello' //.css 생략했는데 css로 인식을 하네?
+
 const MyClock = () => { //클래스 컴포넌트에서 이미 쓰고있어서 꼭 클래스네임으로 해
     return (
         <main className="container">
--- src/02/MyClockTime.js
+++ src/02/MyClockTime.js
@@ -1,3 +1,19 @@
+//1. useState import 리액트에서 유즈 스테이트 갖고오기
+//2. 일반 변수를 state 변수로 선언을 해줘야한다. const[] -> 배열이 된다.
+// const[] = ustState(); (유즈스테이트)나한테 관리 맡기려 하면, 내가 주는건(리턴해주는건) 2개야. 첫번째는 스테이트변수 이름.
+// myTime을 써라~. myTime = ~ 이런식으론 이제 바꿀수 없엉~ 함수명으로 써. setMytime
+// const [mytime, setMytime] = useState(new Date().toLocaleTimeString()); 초기값을 넣어줭. 근데 유즈 스테이트 했는데 안바뀌네?
+// 
+// 콘솔로 찍어보니까 콘솔에서만 바껴.
+//3. 1초에 한번씩 state 변수 변경
+// 1초에 한번씩 바뀌는걸로 제어를 해줘야해->setInterval 함수를 써줘야 한다. 자바스크립트 함수.
+//콜백 함수가 와야해
+// setInterval(()=>{},1000) 1000ms동안 무언걸 해줄거야!
+// myTime을 바꿀거야.
+// myTime = new Date().toLocaleTimeString(); 이렇게 이제 바꿀수가없엉!!! state는 할당연산자를 이용해서 바꿀수가 없다!!! 스테이트의 규칙
+// setMyTime. 함수로서 바꿔줘야해
+// setMyTime(new Date().toLocaleTimeString());
+
 import { useState } from "react";
 
 
