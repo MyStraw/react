b257f61c7af7ae2a282c6d2476f4a79ce47bffbb
MyStraw <kikongdosa@gmail.com>
2023-05-25 11:59:36 +0900

    5월25일 거의완성.

--- src/101/Fcst.js
+++ src/101/Fcst.js
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
+import code from "./getcode.json"; //21. 코드 가져오자
 
 const Fcst = () => {
     //날짜 주소 갖고오는거. 어느 시점에 fetch 던질까? 컴포넌트 페이지 첫 로드될때 페치 던지고 싶을때 그때 사용하는 훅이 이펙트 아닌가?
@@ -21,15 +22,35 @@ const Fcst = () => {
     // 8.1 이건 초기값 아무것도. 뭔지 모르기때문에. useState([]) 이거하면 배열로 초기값 줄수있다.
 
     //13.
-    const [trTags,setTrTags] = useState(); //items가 undefined 될 경우가 있었다. 다시 12번 가서 items&&tags
+    const [trTags, setTrTags] = useState(); //items가 undefined 될 경우가 있었다. 다시 12번 가서 items&&tags
     //15. 아이템 가져왔으니 이제 trtags 바꿀수있네. items가 배열이다. 안에 항목을 object로 가지고 있다. 10개의 object로. 10개의 tr 만들어야해. 행을 화면에 뿌려야 해. 10개를 다 돌아야. 
 
+    //26. opTags도 바껴야지
+    const [opTags, setOptags] = useState();
 
+    //30. ref를 써보자. target.value말고
 
+    const sel = useRef();
 
 
     useEffect(() => { //프로미스 객체 생성. 이 방식으로. url 던졌다가 다시 올때. 이 시점에-> .then((resp)) // 1.2.3. 순서로 가봐.
         //2. 물음표까지가 엔드포인트. 물음표 부터 요청변수, &는 여러개의 요청변수
+
+        //23. 코드가 배열이었다.
+        // let tempcd = code.filter((i) => i["예보구분"] === "초단기예보");
+        //  console.log('tempce', tempcd);
+
+
+        //24. 이제 이걸 셀렉트박스에 해보자        
+
+
+        //22. 콘솔에 찍어보자 코드.              
+        // console.log('code', code); //초단기 예보인것만 골라와야. 필터링.
+
+
+
+
+
         let url = 'https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst?' //요청 url이다.
 
         url = url + 'serviceKey=9IZgTnv%2FlgPK2c%2BJcMk4qKiZfz98OMWrRS4OExS9S%2BVHYm4Axmh%2BUzFH4I0UdNByGrTIfzoyNTSshiljEkvcDw%3D%3D';
@@ -52,6 +73,23 @@ const Fcst = () => {
             //.then((data) => console.log(data)) // 6. 이걸로 F12 보니 response.body.items.item만 필요하단게 보인다.
             .then((data) => setItems(data.response.body.items.item)) //9. 여기까지 들어가야 배열이 나온다. 페치 끝나면 items가 바뀌겠지~ 이게 useEffect 디펜던시 어레이에 items 넣어봐.
             .catch((err) => console.log(err)); //에러 발생할수 있으니 에러를 또 캐치해 내기 위해.
+
+
+        //23. 코드가 배열이었다.
+        //25. 여기로 옮겼다. 
+        let tempcd = code.filter((i) => i["예보구분"] === "초단기예보");
+        tempcd = tempcd.map((i, idx) =>
+            <option key={i["항목값"]} value={i["항목값"]}>{(i["항목명"])}{(i["항목값"])}</option>
+        );
+
+
+        //27.여기에 실행되게. //이제 키값 에러 나니까 키값 주자 바로위에. //그리고 이제 클릭할때마다 아래 테이블 바껴야지.
+        setOptags(tempcd);
+        console.log('tempce', tempcd);
+
+        //28. 이제 셀렉트가 바뀔때마다 아래 테이블이 변해야해. onchange = showItem. 사용자정의 함수 만들어.
+
+
     }, []);
 
 
@@ -59,24 +97,26 @@ const Fcst = () => {
 
     useEffect(() => {
         //14. 아이템이 없을때 안나타내도록 거르기. 아까 items에 undefined 되는게 있었는데 이제 없어진다.
-        if(items === undefined) return;
+   //     if (items === undefined) return;
+
 
         //16. 오브젝트를 다 돌아야 한다. 하나 만들때마다 <tr>이 생겨야 한다. 4개 항목이 있는 tr. jsx는 리턴에 태그가 들어간다.i는 오브젝트 // tr이 10개 들어간다.
         //18. 키값을 줘야해. tr key= i.category + i.fcstDate 이런거 해도 되지만 idx 를 해버리면 유일(오름차순 자연수)
-        let temp = items.map((i, idx) =>
-        <tr key={i.category + idx}> 
-            <td>{i.category}</td>
-            <td>{i.fcstDate}</td>
-            <td>{i.fcstTime}</td>
-            <td>{i.fcstValue}</td>
-        </tr>        
-        );
-
-        console.log('items', items); //10.undefined가 찍힌다. 없을수도 있으니까. 안에 있는걸 내가 가지고 올거야. 화면에다가 뿌릴겨. 이제 이걸 return에 넣어야지. 데이터 갖고오기만, 뿌리는건 return.
-        console.log('temp', temp); //18. 콘솔 확인.
-        setTrTags(temp); //17. 이제 바뀐값이 들어가야 하니까 여기서 set을 해주고. 
         
-    }, [])
+      
+
+
+    //    let temp = items.map((i, idx) =>
+    //         <tr key={i.category + idx}>
+    //             <td>{tempcd["항목명"]}</td>
+    //             <td>{i.fcstDate}</td>
+    //             <td>{i.fcstTime}</td>
+    //             <td>{i.fcstValue}{tempcd["단위"]}</td>
+    //         </tr>
+    //     );
+
+
+    }, [items]) //items 가져올때마다 뿌림
 
     //11. 메인 바꾼다 이제. 피코에서 테이블 형태도 갖고오고. hwp 보고 응답메세지 보고. items에 뭐있는지도 봐. basedate = 발표일자 이런거 봐.
     //12. item에 10개 tr로 10개 생겨야된다. jsx에 문법 들어가야. {trTags}뭔가 바뀔때마다 여기에 뭐가 들어가겠지. 그래서 state변수 썼다. 13가셈. 위로.
@@ -90,22 +130,77 @@ const Fcst = () => {
 
 
 
+
+    //select는 사용자 정의 함수. form으로 싸.
+
+    // 29. 사용자 정의 = select change //form은 막 클릭하면 재렌더링 일어난다. e.preventDefalut(); select로 가져오는방법 여러가지.
+    // 30. 위에 유즈이펙트에 있던 if~부터 다 갖고와. 셀렉트 할때마다 바껴야 하니까 이젠. const sel = useRef(); 배운걸 써보자.
+    // 33. 하늘상태. hwp 17p 있는걸로 예보값의 코드값을 맑음(1), 구름많음(3), 흐림(4) 이걸로 해보자.
+    // 
+    const showItem = (e) => {
+        e.preventDefault();
+        // console.log('sel', e.target.value); //이 방법
+        console.log('sel', sel.current.value); //"항목값골랐다."
+
+        //31. 여기 복사 붙여넣기 해서 , temp, tempcd, 이걸 왜했는지 다시 이해.
+        if (items === undefined) return;
+        let temp = items.filter((i) => i.category === sel.current.value);
+        let tempcd = code.filter((i) => i["예보구분"] === "초단기예보" &&
+        i["항목값"] === sel.current.value
+        ); 
+        tempcd = tempcd[0]; //32.까지.
+
+
+        temp = items.map((i, idx) =>
+            <tr key={i.category + idx}>
+                <td>{tempcd["항목명"]}</td>
+                <td>{i.fcstDate}</td>
+                <td>{i.fcstTime}</td>
+                <td>{i.fcstValue}{tempcd["단위"]}</td>
+            </tr>
+        );
+
+        console.log('items', items); //10.undefined가 찍힌다. 없을수도 있으니까. 안에 있는걸 내가 가지고 올거야. 화면에다가 뿌릴겨. 이제 이걸 return에 넣어야지. 데이터 갖고오기만, 뿌리는건 return.
+        console.log('temp', temp); //18. 콘솔 확인.
+        setTrTags(temp); //17. 이제 바뀐값이 들어가야 하니까 여기서 set을 해주고. 
+
+
+
+
+    }
+
+
+
     return (
         <main className="container">
-            <article>
-                <header><h1>기상청 초단기예보</h1></header>
-                <table>
-                    <thead>
-                        <tr>
-                            <th scope="col">자료구분코드</th>
-                            <th scope="col">예측일자</th>
-                            <th scope="col">예측시간</th>
-                            <th scope="col">예보 값</th>                          
-                        </tr>
-                        {items && trTags}
-                    </thead>
-                </table>
-            </article>
+            <form>
+                <article>
+                    <header>
+
+                        <div className="grid">
+                            <div><h1>기상청 초단기예보</h1></div>
+                            <div>
+                                <select ref={sel} id='sel' name='sel' onChange={showItem}>
+                                    <option val=''>선택</option>
+                                    {opTags}
+                                </select>
+                            </div>
+                        </div>
+
+                    </header>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th scope="col">자료구분코드</th>
+                                <th scope="col">예측일자</th>
+                                <th scope="col">예측시간</th>
+                                <th scope="col">예보 값</th>
+                            </tr>
+                            {items && trTags}
+                        </thead>
+                    </table>
+                </article>
+            </form>
         </main>
     )
 }
